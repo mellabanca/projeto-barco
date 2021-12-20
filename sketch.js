4966e489803224a0a496b6bc772db48b0b5a0f06
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-20 17:57:51 -0300

    atualizado

--- sketch.js
+++ sketch.js
@@ -1,14 +1,45 @@
+var sea,ship;
+var seaImg,shipImg;
 
 function preload(){
-
+  seaImg = loadImage("sea.png");
+  //shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                          //  "ship-2.png","ship-1.png");
+ // shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-2.png");
+  //shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                           // "ship-1.png","ship-1.png");
 }
 
 function setup(){
   createCanvas(400,400);
+  background("blue");
+
+  // Movendo o fundo
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
   
 }
 
 function draw() {
-  background("blue");
- 
+  background(0);
+  sea.velocityX = -3;
+
+  
+  //código para redefinir o fundo
+  if(sea.x < 0){
+   // sea.x = 0;
+   // sea.x = sea.width;
+   // sea.x = sea.width/8;
+   // sea.x = sea.height;
+  }
+    
+  drawSprites();
 }
